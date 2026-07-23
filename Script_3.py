36d07be3dedc09f3cb7e6fac5c15215f04ccefaa
Skyfoxfelder <168836139+Skyfoxfelder@users.noreply.github.com>
2026-07-23 12:33:57 +0000

    v

---
+++ Script_3.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils_rule import triggerRule
+
+
+with DAG(
+    dag_id="dag_exercice_2",
+    start_date=datetime(2026, 23, 7),
+    schedule=None,
+    catchup=False,
+    tags=["guide"],
+) as dag:
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date"
+    )
+    babou = BashOperator(
+        task_id="babou",
+        bash_command="echo 'Tache 1 : OK'"
+        #trigger_rule=triggerRule.ONE_SUCCESS
+    )
+    dodo = BashOperator(
+        task_id="dodo",
+        bash_command="echo 'Tache 2 : OK'"
+        #trigger_rule=triggerRule.ONE_FAILED
+    )
+    Pipo = BashOperator(
+            task_id="pipo",
+            bash_command="echo 'Tache 2 : OK'"
+    )
+    toto = BashOperator(
+            task_id="toto",
+            bash_command="echo 'Tache 2 : OK'"
+    )
+
+    hello >> [babou, Pipo] >> [dodo, toto]
