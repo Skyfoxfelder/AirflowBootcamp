be96f879e379bdca88a1cf9071e159ffe816246a
Skyfoxfelder <168836139+Skyfoxfelder@users.noreply.github.com>
2026-07-23 13:45:44 +0200

    Create Scripe_2.py

---
+++ Scrip_2.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+ 
+with DAG(
+    dag_id="dag_exercice",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["guide"],
+) as dag:
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date"
+    )
+    hello = BashOperator(
+        task_id="ex_1",
+        bash_command="echo 'Tache 1 : OK"
+    )
+    hello = BashOperator(
+        task_id="ex_2",
+        bash_command="echo 'Tache 2 : OK"
+    )
+hello >> ex_1 >> ex_2
